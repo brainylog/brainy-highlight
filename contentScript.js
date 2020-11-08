d7c218108bd2e562a9d589868fabee01a2a1e8a8
thehamzarocks <thehamzarocks@gmail.com>
2020-11-08 19:39:29 +0530

    POSTing slected text to notes service

--- contentScript.js
+++ contentScript.js
@@ -2,6 +2,17 @@ alert("Hello I'm a content script");
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   str = window.getSelection().toString();
   alert("You selected " + str);
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://5q71jg.deta.dev/extensionFiles/", true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      // JSON.parse does not evaluate the attacker's scripts.
+
+      alert(xhr.responseText);
+      // var resp = JSON.parse(xhr.responseText);
+    }
+  };
+  xhr.send(JSON.stringify({text: str}));
   if (request.greeting == "hello") sendResponse({ farewell: "goodbye" });
 });
 // chrome.runtime.onMessage.addListener(function (message, callback) {
